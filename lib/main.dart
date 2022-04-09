703db1adcd8384f31126cd2cf82b3c5f6772af9f
Ponlakit Konkrathok <Ponlakit13@hotmail.com>
2022-04-09 15:49:21 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:recipes/screens/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Recipes',
+      theme: ThemeData(
+        //backgroundColor: Colors.red,
+        primaryColor: Color(0xff263341),
+        accentColor: Color(0xff8DB646),
+      ),
+      home: HomePage(),
+    );
+  }
+}
